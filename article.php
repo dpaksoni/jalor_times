0f0db33b7a025706825c8fca4c3b767a85fae624
Pravesh Jain <prvsh12345@gmail.com>
2016-12-09 23:37:05 +0530

    article ka page

---
+++ article.php
@@ -0,0 +1,156 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<title>CA Agarwals</title><link rel="shortcut icon" href="images/ca.png">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Hosting City Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstrap-css -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!--// bootstrap-css -->
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--// css -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- font -->
+<link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+<!-- //font -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/SmoothScroll.min.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+</head>
+<body>
+	<!-- header-top -->
+	<div class="header-top">
+		<div class="container">
+			<div class="w3layouts-address">
+				<ul>
+					<li><i class="fa fa-mobile" aria-hidden="true"></i>  +91 9414584058</li>
+					<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@caagarwals.com"> info@caagarwals.com</a></li>
+				</ul>
+			</div>
+			<div class="agileinfo-social-grids">
+				<ul>
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+					
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!-- //header-top -->
+	<!-- header -->
+	<div class="header">
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						<div class="w3layouts-logo">
+							<h1><a href="index.html">CA <span>Agarwals</span></a></h1>
+						</div>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						<nav>
+							<ul class="nav navbar-nav">
+							
+								<li><a href="index.php">Home</a></li>
+								<li><a href="about.php" class="hvr-sweep-to-bottom">About</a></li>
+								<li><a href="#" class="dropdown-toggle hvr-sweep-to-bottom" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services<span class="caret"></span></a>
+									<ul class="dropdown-menu">
+									<li><a class="hvr-sweep-to-bottom" href="service.php">Corporate Law and Other Allied Services</a></li>
+										<li><a class="hvr-sweep-to-bottom" href="tax.php">Tax</a></li>
+										
+										<li><a class="hvr-sweep-to-bottom" href="consult.php">
+Transaction Advisory & Consulting
+</a></li>
+											</ul>
+								</li>
+								<li class="active"><a href="article.php" class="hvr-sweep-to-bottom">ArticleShip</a></li>
+								
+								<li><a href="career.php" class="hvr-sweep-to-bottom">Career</a></li>
+								<li  ><a href="mail.php" class="hvr-sweep-to-bottom">Mail Us</a></li>
+							</ul>
+						</nav>
+					</div>
+					<!-- /.navbar-collapse -->
+				</nav>
+			</div>
+	</div>
+	<!-- //header -->
+	<!-- about-heading -->
+	<div class="about-heading">
+		<h2>Article<span>Ship</span></h2>
+	</div>
+	<!-- //about-heading -->
+	<div class="faq">
+			<div class="container">
+						<h3 class="hdg">Article Ship</h3>
+						<div class="well">
+							Your achievement is our accomplishment, so we offer you the prospects to attain your best.
+						</div>
+						<div class="well">
+							Be part of an organization where you can serve some of the leading and most esteemed brands in the market. Work with outstanding team to tackle new challenges every day.
+						</div>
+						<div class="well">
+								At Agarwal A R & Associates, Chartered Accountants, we’re dedicated to helping you grow and develop and add value to your professional career.
+						</div>
+						<div class="well">
+							Please email your Resume for considerations at info@caagarwals.com
+						</div><div class="well">
+								Put Your Career on A Fast Track.
+						</div>
+						<div class="well">
+								Throughout your career, you will be challenged by the work and by the people on your team and at the clients we serve. You will be constantly learning. Your ideas will be welcomed in an atmosphere of collaboration and teamwork. Your individuality will be recognised and encouraged to be yourself.
+				
+						</div>
+							</div>
+	</div>
+	<!-- footer -->
+	<?php
+	include'footer.php';
+	?>
+	<!-- //footer -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+	<!-- //here ends scrolling icon -->
+</body>	
+</html>
